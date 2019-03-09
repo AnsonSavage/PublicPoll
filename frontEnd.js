e0511f28ecca56fa218589f7a13083298f295225
Anson Savage <ansonsavage@gmail.com>
2019-03-09 15:30:36 -0700

    Ready to submit version 1, no comments

--- frontEnd.js
+++ frontEnd.js
@@ -26,28 +26,41 @@ let app = new Vue({
       this.optionsInCreation.push("");
     },
     addPoll() {
-      let poll = {
-        username: this.username,
-        question: this.questionInCreation,
-        options: [],
-        comments: [],
-        addComment: comment => {
-          this.comments.push(comment)
-        },
+      if (!(this.username === '' | this.questionInCreation === '' | this.optionsInCreation[0] === '' | this.optionsInCreation[1] === '')) {
+        let poll = {
+          username: this.username,
+          question: this.questionInCreation,
+          options: [],
+          // comments: [],
+          // addComment: comment => {
+          //   this.comments.push(comment)
+          // },
+          total: 0,
+          date: moment().format('MMMM Do YYYY, h:mm:ss a'),
+        }
+        this.optionsInCreation.forEach(option => {
+          if (option !== "") {
+            let newOption = {}; //Create a new, empty object
+            newOption[option] = 0;
+            console.log(newOption[option], option);
+            poll.options.push(newOption);
+            console.log(poll.options);
+          }
+        });
+        this.polls.unshift(poll);
+        //Clear out variables
+        this.username = "";
+        this.questionInCreation = "";
+        this.optionsInCreation = ["", ""];
+        console.log("We just added a poll!");
+      } else {
+        console.log("Insufficient information to add a poll!")
       }
-      this.optionsInCreation.forEach(option => {
-        let newOption = {}; //Create a new, empty object
-        newOption[option] = 0;
-        console.log(newOption[option], option);
-        poll.options.push(newOption);
-        console.log(poll.options);
-      });
-      this.polls.push(poll);
-      //Clear out variables
-      this.username = "";
-      this.questionInCreation = "";
-      this.optionsInCreation = ["", ""];
-      console.log("We just added a poll!");
+    },
+    addCount(poll, option) {
+      poll.total++;
+      //Increment the count associated with that particular value
+      option[Object.keys(option)[0]]++; //There's got to be a better way to do this, but oh well.
     },
     // addComment(item) {
     //   console.log(item);
