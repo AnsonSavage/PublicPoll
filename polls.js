dfffa186380235ba03b2511ab2ee445bef5dcd3e
Anson Savage <ansonsavage@gmail.com>
2019-03-30 15:21:58 -0600

    We can now post, put, and get from the server! I'm going to work on some optimizations now.

--- polls.js
+++ polls.js
@@ -6,13 +6,16 @@ const router = express.Router(); //We need a router to rout all our api request!
 const pollSchema = new mongoose.Schema({ //Maps to a MongDB collection
   username: String, //Notice that, as of right now, we are storing the username in the Poll itself. What I plan to do is to have each user object have an array of ids which point to the polls they have made.
   questionText: String,
-  date: Date,
-  options: [{
-    optionText: String,
-    votes: Number
-  }],
+  date: Date, //We could store this as a Date object, but we're already sending the date over as a string... IDK what's better practice, we can change this later.
+  optionsText: [String], //This is how the stuff will be stored in the database, but when it's converted to JSON, it'll get slammed back into one object.
+  optionsVotes: [Number]
   //comments: [{commentUser: String, commentText: String}] //Go ahead and implement this once we get comments working.
 });
+
+// pollSchema.methods.toJSON = async () => { //I'm making this so we can turn the object ids into actual objects.
+//   date
+// };
+
 
 const Poll = mongoose.model('Poll', pollSchema); //We have to convert the schema into a model in order to begin working with it. Instances of this model become documents
 
@@ -26,18 +29,36 @@ router.get("/", async (request, response) => { //Again, we don't have to use "/a
 });
 
 router.post("/", async (request, response) => {
+  console.log(request.body.options); //These are the options sent across
   const poll = new Poll({
     username: request.body.username, //I'm pretty sure that this stuff already went through the bodyParser.
     questionText: request.body.questionText,
-    date: request.body.date,
-    options: request.body.options
+    date: new Date(),
+    optionsText: request.body.optionsText, //I hope you see the importance of keeping these two arrays in sync!
+    optionsVotes: request.body.optionsText.map(option => 0) //For each option in the optionsText, initialize the vote to zero.
   });
   try {
     await poll.save(); //Try to save this in the database as a new document
+    console.log("We added a poll to the server!");
     return response.send(poll); //Send back the poll!
   } catch (error) {
     console.log(error);
     return response.sendStatus(500); //Something really bad happened! We can't save to our server!
+  }
+});
+
+router.put("/:id", async (request, response) => {
+  try {
+    await Poll.updateOne({
+      _id: request.params.id,
+    }, {
+      optionsVotes: request.body.optionsVotes,
+    });
+    response.sendStatus(200);
+  } catch (error) {
+    console.log("Couldn't update the vote count on the object!");
+    console.log(error);
+    response.sendStatus(500);
   }
 });
 
--- server.js
+++ server.js
@@ -7,12 +7,14 @@ app.use(bodyParser.urlencoded({ //Only parses urlencoded bodies,
   extended: false //We tell it that the value in the string-value pairs can only be a string or an array (if we set this to true, it could be any type)
 }));
 
-app.use(express.static("../public")); //Tells express to serve static files from the public directory
+app.use(express.static("./public")); //Tells express to serve static files from the public directory
 const mongoose = require("mongoose"); //Import mongoose!
 
 mongoose.connect("mongodb://localhost:27017/polls", {
   useNewUrlParser: true //This basically just tells Mongoose that we want to use the new url parser, which requires that we specify a port. The old one is apparently depreciated.
 }) //We now connect to the Mongo polls database.
 
-const tickets = require("./polls.js"); //We want to use the poll.js file in our current directory.
-app.use("/api/polls", polls.js); //Thus, if we get any path starting with /api/polls, we know we should use the poll.js code
+const polls = require("./polls.js"); //We want to use the poll.js file in our current directory.
+app.use("/api/polls", polls); //Thus, if we get any path starting with /api/polls, we know we should use the poll.js code
+
+app.listen(3001, () => console.log("The server is listening on port 3001, yahoo"));
